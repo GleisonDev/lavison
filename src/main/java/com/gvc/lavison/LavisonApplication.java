4d403a93f81124886bd59842396bc2716fd2454c
Gleison <gleisondev5690@gmail.com>
2023-02-04 17:43:53 -0300

    first commit

---
+++ src/main/java/com/gvc/lavison/LavisonApplication.java
@@ -0,0 +1,13 @@
+package com.gvc.lavison;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LavisonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LavisonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gvc/lavison/LavisonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gvc.lavison;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LavisonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
